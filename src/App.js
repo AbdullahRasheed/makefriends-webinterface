b1199b2e6a63257c2ad289aa0e54d8aa2d5b7657
AbdullahRasheed <ahrasheed22@ksu.edu>
2022-09-25 14:21:05 -0500

    Front page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import './App.css';
-import SignupForm from './Signup';
-import LoginForm from './Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>MakeFriends</h1>
-      </header>
-      <LoginForm className="login-form"/>
-      <SignupForm className="signup-form"/>
+
+    <div className="title-page">
+      <LeftMainContent />
+      <RightMainContent />
+    </div>
+
+  );
+}
+
+function LeftMainContent() {
+  return (
+    <div className="leftside">
+      <div>
+        <h1 className="title">MakeFriends</h1>
+        <p className="title-support">Something cool here</p>
+      </div>
+    </div>
+  );
+}
+
+function RightMainContent() {
+  return (
+    <div className="rightside">
+      <div className="main-right-content">
+        <h1>Join now!</h1>
+        <div className="cont-form">
+          <button type="button">Log in</button>
+          <button type="button">Sign up</button>
+        </div>
+      </div>
     </div>
   );
 }
