37f3e1a47da886f9bd328579639787464ef3263d
AbdullahRasheed <ahrasheed22@ksu.edu>
2022-09-25 14:55:42 -0500

    Redirects

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, useNavigate} from 'react-router-dom';
+import LoginForm from './Login';
+import SignupForm from './Signup';
 
 function App() {
   return (
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<TitlePage />} />
+        <Route exact path='/login' element={<LoginForm />} />
+        <Route exact path='/signup' element={<SignupForm />} />
+      </Routes>
+    </Router>
+  );
+}
 
+function TitlePage(){
+  return (
     <div className="title-page">
       <LeftMainContent />
       <RightMainContent />
     </div>
-
   );
 }
 
@@ -23,13 +36,15 @@ function LeftMainContent() {
 }
 
 function RightMainContent() {
+  const navigate = useNavigate();
+
   return (
     <div className="rightside">
       <div className="main-right-content">
         <h1>Join now!</h1>
         <div className="cont-form">
-          <button type="button">Log in</button>
-          <button type="button">Sign up</button>
+          <button type="button" onClick={() => navigate("/login")}>Log in</button>
+          <button type="button" onClick={() => navigate("/signup")}>Sign up</button>
         </div>
       </div>
     </div>
